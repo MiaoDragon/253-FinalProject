1204ce3b8cb6523e83ec3dbee6130fd17f6fe9dc
MiaoDragon <innocentmyl@gmail.com>
2019-03-12 14:09:42 -0700

    reduce number of episodes

--- pendulum.sh
+++ pendulum.sh
@@ -1,4 +1,4 @@
-python3 train.py --env Pendulum-v0 --max_epi 2500 --max_iter 200 \
+python3 train.py --env Pendulum-v0 --max_epi 1000 --max_iter 100 \
 --save_epi 100 --memory_capacity 1000 --learning_rate 0.01 \
 --obs_num 4 --model_path ../model/baseline.pkl --seed 1 --start_std 5. \
 --final_std 0.1 --std_decay_epi_ratio 0.7
