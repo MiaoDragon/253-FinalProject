184fcdf05b932f01531a3d98032d6df8df4088d6
MiaoDragon <innocentmyl@gmail.com>
2019-03-13 15:52:56 -0700

    change setting to smaller

--- pendulum.sh
+++ pendulum.sh
@@ -1,4 +1,4 @@
-python3 train.py --env Pendulum-v0 --max_epi 1000 --max_iter 100 \
+python3 train.py --env Pendulum-v0 --max_epi 500 --max_iter 100 \
 --save_epi 100 --memory_capacity 1000 --learning_rate 0.01 \
---obs_num 4 --model_path ../model/baseline.pkl --seed 1 --start_std 0.1 \
+--obs_num 4 --model_path ../model/baseline.pkl --seed 1 --start_std 2. \
 --final_std 0.1 --std_decay_epi_ratio 0.7
