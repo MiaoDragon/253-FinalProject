206370f233a6b5ac5d35901b457bf25c6c0452cc
MiaoDragon <innocentmyl@gmail.com>
2019-03-13 15:43:42 -0700

    print parameters

--- train.py
+++ train.py
@@ -56,7 +56,8 @@ def main(args):
     std_decay = (start_std - final_std) / std_decay_epi
     std = start_std
     policyNet.update_std(std)
-    for param in policyNet.parameters():
+    for name, param in policyNet.named_parameters():
+        print(name)
         print(param)
     # may consider adding random generation of data
     for i_episode in range(args.max_epi):
