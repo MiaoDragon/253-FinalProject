e96921c4781f3edb43077f3b5e3874479e131ed1
MiaoDragon <innocentmyl@gmail.com>
2019-03-14 13:45:16 -0700

    change iterations to 1000

--- pendulum.sh
+++ pendulum.sh
@@ -1,3 +1,3 @@
-python3 train.py --env Pendulum-v0 --max_epi 5000 --max_iter 100 \
+python3 train.py --env Pendulum-v0 --max_epi 5000 --max_iter 1000 \
 --save_epi 100 --memory_capacity 100 --learning_rate 0.001 \
 --obs_num 4 --model_path ../model/baseline.pkl --seed 1 --use_cnn 0
