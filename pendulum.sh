a9e9fe94e367471c944540eac1043c52864a50fd
MiaoDragon <innocentmyl@gmail.com>
2019-03-14 11:29:02 -0700

    should use 0 instead of False

--- pendulum.sh
+++ pendulum.sh
@@ -1,4 +1,4 @@
 python3 train.py --env Pendulum-v0 --max_epi 500 --max_iter 100 \
 --save_epi 100 --memory_capacity 1000 --learning_rate 0.01 \
 --obs_num 4 --model_path ../model/baseline.pkl --seed 1 --start_std 2. \
---final_std 0.15 --std_decay_epi_ratio 0.7 --use_cnn False
+--final_std 0.15 --std_decay_epi_ratio 0.7 --use_cnn 0
